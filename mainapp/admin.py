9c7199614ef39c8c6e2564b5f5eb3f1bb4c1348b
vlgreb <vlgrebennikov@mail.ru>
2021-03-08 17:54:03 +0300

    make image size limit

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import *
 
 from PIL import Image
@@ -8,20 +9,26 @@ from PIL import Image
 
 class NotebookAdminForm(ModelForm):
 
-    MIN_RESOLUTION = (400, 400)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = 'Загружайте изображеие с минимальным разрешением {}*{}'.format(
-            *self.MIN_RESOLUTION
+        self.fields['image'].help_text = mark_safe(
+            '<span style="color:red; font-size: 14px;">Загружайте изображеие с минимальным разрешением {}*{}</span>'.format(
+                *Product.MIN_RESOLUTION
+            )
         )
 
     def clean_image(self):
         image = self.cleaned_data['image']
         img = Image.open(image)
-        min_height, min_width = self.MIN_RESOLUTION
+        min_height, min_width = Product.MIN_RESOLUTION
+        max_height, max_width = Product.MAX_RESOLUTION
+        if image.size > Product.MAX_IMAGE_SIZE:
+            raise ValidationError('Размер изображения не должен превышать 3MB')
         if img.height < min_height or img.width < min_width:
             raise ValidationError('Разрешение загруженного изображения меньше минимального')
+        if img.height > max_height or img.width > max_width:
+            raise ValidationError('Разрешение загруженного изображения больше допустимого')
         return image
 
 
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,9 +1,19 @@
+from PIL import Image
+
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
+
+
+class MinResolutionErrorExeption(Exception):
+    pass
+
+
+class MaxResolutionErrorExeption(Exception):
+    pass
 
 
 class LatestProductsManager:
@@ -43,6 +53,10 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    MIN_RESOLUTION = (400, 400)
+    MAX_RESOLUTION = (800, 800)
+    MAX_IMAGE_SIZE = 3145728
+
     class Meta:
         abstract = True
 
@@ -55,6 +69,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        image = self.image
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        max_height, max_width = self.MAX_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise MinResolutionErrorExeption('Разрешение загруженного изображения меньше минимального')
+        if img.height > max_height or img.width > max_width:
+            raise MaxResolutionErrorExeption('Разрешение загруженного изображения больше допустимого')
+        return image
 
 
 class Notebook(Product):
