525fdabe7d1152a7c2a4bc317918ca3935086204
vlgreb <vlgrebennikov@mail.ru>
2021-03-08 18:28:28 +0300

    made cropping an image

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -13,23 +13,24 @@ class NotebookAdminForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['image'].help_text = mark_safe(
-            '<span style="color:red; font-size: 14px;">Загружайте изображеие с минимальным разрешением {}*{}</span>'.format(
-                *Product.MIN_RESOLUTION
+            """<span style="color:red; font-size: 14px;">При загрузке изображения больше {}*{} оно будет обрезано</span>
+            """.format(
+                *Product.MAX_RESOLUTION
             )
         )
 
-    def clean_image(self):
-        image = self.cleaned_data['image']
-        img = Image.open(image)
-        min_height, min_width = Product.MIN_RESOLUTION
-        max_height, max_width = Product.MAX_RESOLUTION
-        if image.size > Product.MAX_IMAGE_SIZE:
-            raise ValidationError('Размер изображения не должен превышать 3MB')
-        if img.height < min_height or img.width < min_width:
-            raise ValidationError('Разрешение загруженного изображения меньше минимального')
-        if img.height > max_height or img.width > max_width:
-            raise ValidationError('Разрешение загруженного изображения больше допустимого')
-        return image
+    # def clean_image(self):
+    #     image = self.cleaned_data['image']
+    #     img = Image.open(image)
+    #     min_height, min_width = Product.MIN_RESOLUTION
+    #     max_height, max_width = Product.MAX_RESOLUTION
+    #     if image.size > Product.MAX_IMAGE_SIZE:
+    #         raise ValidationError('Размер изображения не должен превышать 3MB')
+    #     if img.height < min_height or img.width < min_width:
+    #         raise ValidationError('Разрешение загруженного изображения меньше минимального')
+    #     if img.height > max_height or img.width > max_width:
+    #         raise ValidationError('Разрешение загруженного изображения больше допустимого')
+    #     return image
 
 
 
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,9 +1,13 @@
+import sys
 from PIL import Image
 
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from io import BytesIO
 
 User = get_user_model()
 
@@ -71,15 +75,26 @@ class Product(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
+        # image = self.image
+        # img = Image.open(image)
+        # min_height, min_width = self.MIN_RESOLUTION
+        # max_height, max_width = self.MAX_RESOLUTION
+        # if img.height < min_height or img.width < min_width:
+        #     raise MinResolutionErrorExeption('Разрешение загруженного изображения меньше минимального')
+        # if img.height > max_height or img.width > max_width:
+        #     raise MaxResolutionErrorExeption('Разрешение загруженного изображения больше допустимого')
         image = self.image
         img = Image.open(image)
-        min_height, min_width = self.MIN_RESOLUTION
-        max_height, max_width = self.MAX_RESOLUTION
-        if img.height < min_height or img.width < min_width:
-            raise MinResolutionErrorExeption('Разрешение загруженного изображения меньше минимального')
-        if img.height > max_height or img.width > max_width:
-            raise MaxResolutionErrorExeption('Разрешение загруженного изображения больше допустимого')
-        return image
+        new_img = img.convert('RGB')
+        resized_new_img = new_img.resize((200, 200), Image.ANTIALIAS)
+        filestream = BytesIO()
+        resized_new_img.save(filestream, 'JPEG', quality=90)
+        filestream.seek(0)
+        name = '{}.{}'.format(*self.image.name.split('.'))
+        self.image = InMemoryUploadedFile(
+            filestream, 'ImageField', name, 'jpec/image', sys.getsizeof(filestream), None
+        )
+        super().save(*args, **kwargs)
 
 
 class Notebook(Product):
